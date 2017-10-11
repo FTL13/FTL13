dcbc0025a519de8b44f2da244432465af62b4da3
duncathan salt <dunc2403@dunc2403.com>
2017-10-11 10:55:31 -0600

    TGUI Compiler PHP script

--- tools/travis/build_tools.sh
+++ tools/travis/build_tools.sh
@@ -7,5 +7,10 @@ if [ "$BUILD_TOOLS" = true ];
 then
     md5sum -c - <<< "49bc6b1b9ed56c83cceb6674bd97cb34 *html/changelogs/example.yml";
     cd tgui && source ~/.nvm/nvm.sh && gulp && cd ..;
+<<<<<<< HEAD
+=======
+    php5.6 -l tools/WebhookProcessor/github_webhook_processor.php;
+    php5.6 -l tools/TGUICompiler.php;
+>>>>>>> c39ba8f... Merge pull request #31536 from tgstation/Cyberboss-patch-2
     python tools/ss13_genchangelog.py html/changelog.html html/changelogs;
 fi;
